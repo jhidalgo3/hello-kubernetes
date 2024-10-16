1397bf5de8372e38d1a524e5e281891f64e0ea77
Jose Maria Hidalgo Garcia <e_jhid01@mercadona.com>
2024-10-16 15:04:49 +0200

    Upgrade npm dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14-alpine
+FROM node:20-alpine
 
 ARG IMAGE_CREATE_DATE
 ARG IMAGE_VERSION
--- app/server.js
+++ app/server.js
@@ -1,10 +1,11 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var { engine } = require('express-handlebars');
+
 var app = express();
 var os = require("os");
 var morgan  = require('morgan');
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.use(express.static('static'));
 app.use(morgan('combined'));
